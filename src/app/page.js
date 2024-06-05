26bd29442c775c1aef05a0fae17119d3eea78f83
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-05 07:21:47 -0500

    Consumiendo API regres para obtener usuarios y mostrarlos

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-export default function Home() {
+async function fetchUsers() {
+    const res = await fetch("https://reqres.in/api/users");
+    const { data } = await res.json();
+    return data;
+}
+
+export default async function HomePage() {
+    const users = await fetchUsers();
+    console.log(users);
     return (
         <main className="">
             <h2>Usuarios</h2>
+            <div>
+                {users.length === 0
+                    ? "No existen Usuarios"
+                    : users.map((user) => (
+                          <div key={user.id}>
+                              <p>{user.first_name}</p>
+                              <p>{user.email}</p>
+                          </div>
+                      ))}
+            </div>
         </main>
     );
 }
