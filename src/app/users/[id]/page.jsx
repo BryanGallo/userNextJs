c5b069690808b9daef6e63af3da544dd48c9813a
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-06 07:33:34 -0500

    Obteniendo Informacion del usuario y mostrandola

--- src/app/users/[id]/page.jsx
+++ src/app/users/[id]/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Hola</div>
-  )
+async function getUsers(id) {
+    const res = await fetch(`https://reqres.in/api/users/${id}`);
+    const { data } = await res.json();
+    console.log(data);
+    return data;
 }
 
-export default page
+const page = async ({ params }) => {
+    const { id } = params;
+    const user = await getUsers(id);
+    return (
+        <div
+            key={user.id}
+            className="flex justify-between items-center bg-teal-600 rounded-md p-4 my-2 space-y-3 cursor-pointer"
+        >
+            <p className="font-bold text-xl">
+                Nombre:
+                <span> {user.first_name}</span>
+            </p>
+            <p className="font-bold text-xl">
+                Apellido:
+                <span> {user.last_name}</span>
+            </p>
+            <p className="font-bold text-xl">
+                Correo:
+                <span> {user.email}</span>
+            </p>
+            <img className="rounded-full w-28" src={user.avatar} />
+        </div>
+    );
+};
+
+export default page;
