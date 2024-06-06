60ff805ba26d3627132a9caf64fe53a303ac5575
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-06 07:27:55 -0500

    Cambiando onClick por link y creando ruta dinamica

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,22 +1,18 @@
-"use client";
+import Link from "next/link";
 
 const User = ({ user }) => {
     return (
-        <div
-            key={user.id}
-            className="flex flex-col justify-center items-center bg-teal-600 rounded-md p-4 my-2 space-y-3"
-        >
-            <p className="font-bold text-xl">{user.first_name}</p>
-            <img className="rounded-full w-36" src={user.avatar} />
-            <button
-                className="bg-teal-800 p-4 rounded-md"
-                onClick={(e) => {
-                    alert("hola");
-                }}
+        <Link href={`/users/${user.id}`}>
+            <div
+                key={user.id}
+                className="flex justify-between items-center bg-teal-600 rounded-md p-4 my-2 space-y-3 cursor-pointer"
             >
-                Ver Info
-            </button>
-        </div>
+                <p className="font-bold text-xl">
+                    {user.id}. {user.first_name}
+                </p>
+                <img className="rounded-full w-28" src={user.avatar} />
+            </div>  
+        </Link>
     );
 };
 
