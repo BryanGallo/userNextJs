50129d29228d581ecc83dff2e9b9d4cf2db4a2e9
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-06 23:35:18 -0500

    Haciendole adaptativo

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default async function HomePage() {
     const users = await fetchUsers();
     console.log(users);
     return (
-        <main className="container mx-auto h-screen w-1/6">
+        <main className="container mx-auto h-screen w-4/6 md:w-3/6">
             <h2 className="text-center text-3xl">Usuarios</h2>
             <div>
                 {users.length === 0
--- src/app/users/[id]/page.jsx
+++ src/app/users/[id]/page.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getUsers(id) {
     const res = await fetch(`https://reqres.in/api/users/${id}`);
     const { data } = await res.json();
@@ -9,23 +11,32 @@ const page = async ({ params }) => {
     const { id } = params;
     const user = await getUsers(id);
     return (
-        <div
-            key={user.id}
-            className="flex justify-between items-center bg-teal-600 rounded-md p-4 my-2 space-y-3 cursor-pointer"
-        >
-            <p className="font-bold text-xl">
-                Nombre:
-                <span> {user.first_name}</span>
-            </p>
-            <p className="font-bold text-xl">
-                Apellido:
-                <span> {user.last_name}</span>
-            </p>
-            <p className="font-bold text-xl">
-                Correo:
-                <span> {user.email}</span>
-            </p>
-            <img className="rounded-full w-28" src={user.avatar} />
+        <div className="p-4">
+            <div
+                key={user.id}
+                className="container mx-auto flex justify-center items-center bg-teal-600 rounded-md p-4 my-2 space-y-3 cursor-pointer space-x-7 w-6/6 md:w-3/6"
+            >
+                <div className="">
+                    <img className="rounded-full w-28" src={user.avatar} />
+                </div>
+                <div>
+                    <p className="font-bold text-xl">
+                        Nombre:
+                        <span> {user.first_name}</span>
+                    </p>
+                    <p className="font-bold text-xl">
+                        Apellido:
+                        <span> {user.last_name}</span>
+                    </p>
+                    <p className="font-bold text-xl">
+                        Correo:
+                        <span> {user.email}</span>
+                    </p>
+                </div>
+            </div>
+            <div className="container mx-auto text-center">
+                <Link href="/">Regresar</Link>
+            </div>
         </div>
     );
 };
